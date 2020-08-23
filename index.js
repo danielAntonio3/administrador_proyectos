068a24d6f534f5bbca617e7d591334f78884340e
danielAntonio3 <49245656+danielAntonio3@users.noreply.github.com>
2020-08-22 20:29:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -33,6 +33,6 @@ app.use('/api/tareas',require('./router/tareas'));
 
 
 // Iniciamos el servirdor
-app.listen(port,'0.0.0.0', ()=>{
+app.listen(port, ()=>{
     console.log(`El servidor esta funcionando en el puerto ${port}`);
 })
